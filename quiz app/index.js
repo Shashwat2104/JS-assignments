e1da8f6a373b77de2fd607cb641b450b3d94f973
Shashwat2104 <Shashwatmahender2104@gmail.com>
2022-10-15 08:29:39 +0530

    new file

---
+++ quiz app/index.js
@@ -0,0 +1,66 @@
+const quizData = [{
+    question: "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
+];
+
+let index = 0 ;
+const questionBox = document.getElementById("questionBox")
+const allInputs = document.getElementById("input[type='radio']")
+
+ const loadQuestion = ()=>{
+
+
+
+
+
+    const data = quizData[index]
+    questionBox.innerHTML = `${index+1})  ${data.question}`
+
+    allInputs[0].nextElementSibling.innerText = data.a
+    allInputs[1].nextElementSibling.innerText = data.b
+    allInputs[2].nextElementSibling.innerText = data.c
+    allInputs[3].nextElementSibling.innerText = data.d
+
+
+
+
+
+
+
+    // console.log(data)
+   
+ }
+
+ loadQuestion()
+
+
+ 
