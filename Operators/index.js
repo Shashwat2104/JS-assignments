77258d610ce5fbb492cfb1bed7d3e09089462f3a
Shashwat2104 <Shashwatmahender2104@gmail.com>
2022-10-15 07:00:18 +0530

    add it now

---
+++ Operators/index.js
@@ -0,0 +1,53 @@
+// let database_username = "abc@gmail.com";
+// let database_password = "ab123@";
+
+
+// let username = "abc@gmail.com";
+// let password = "ab1r23@";
+
+
+
+// database_username === username && database_password === password ? console.log("Enter") : console.log("wrong ")
+// if(database_username==username){
+//   if(database_password==password)
+//   {
+//     console.log("Enter")
+//   } else{
+//     console.log("wrong Password")
+//   }
+//     }
+// else {
+//   console.log("Wrong usern name")
+// }
+//  let char = "e" 
+
+// if (char === "a" || char === "e" &&||  char === "i" &&|| char === "o" || char === "u"){
+//   console.log("Vowel")
+// }
+
+
+if(18%2==0){
+
+if(9%3==0){
+
+if(4%3==0){
+
+console.log("Hello!");
+
+} else {
+
+console.log("Bye!");
+
+}
+
+} else {
+
+console.log("Hello Again!");
+
+}
+
+} else {
+
+console.log("Bye Again!");
+
+}
